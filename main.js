e9336e9ff136167493f81077f4a968bf0537ad8f
Ron Sims II <ronsims2@gmail.com>
2023-04-29 18:46:22 -0400

    Updated demo to pass image buffer to wasm resize.

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-import init, { greet } from 'wasm-image-editor'
+import init, { greet, resize_image } from 'wasm-image-editor'
+import axios from 'axios'
 init().then(x => {
-    greet('foo')
+    const uploadElement = document.querySelector('#image_upload')
+    uploadElement.addEventListener('change', (e) => {
+        if (e.target.files.length) {
+            const fileUrl = URL.createObjectURL(e.target.files[0])
+            axios.get(fileUrl, { responseType: 'blob'}).then(data => {
+                return data.data.arrayBuffer()
+            }).then(data => {
+                const dataArray = new Uint8Array(data)
+                const resizedImage = resize_image(dataArray, 0.5)
+                console.log(resizedImage)
+            })
+        }
+    })
+    greet('foo bar baz')
 })
 
 
